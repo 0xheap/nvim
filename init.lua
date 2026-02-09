ab138673ec643e87f4d8764fa87080231d82ed94
Abdessamad El Bouazzaoui <abdessel@e1r2p5.1337.ma>
2026-02-09 23:47:56 +0100

    config

--- init.lua
+++ init.lua
@@ -111,6 +111,49 @@ require("lazy").setup({
     end
   },
   {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        cmdline = {
+          enabled = true,
+          view = "cmdline_popup",
+          format = {
+            cmdline = { pattern = "^:", icon = ">", lang = "vim" },
+            search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
+            search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
+            filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+            lua = { pattern = "^:%s*lua%s+", icon = "☾", lang = "lua" },
+            help = { pattern = "^:%s*he?l?p?%s+", icon = "?" },
+          },
+        },
+        messages = {
+          enabled = true,
+          view = "notify",
+        },
+        popupmenu = {
+          enabled = true,
+          backend = "nui",
+        },
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = false,
+          command_palette = true,
+          long_message_to_split = true,
+        },
+      })
+    end
+  },
+  {
     "goolord/alpha-nvim",
     config = function()
       local alpha = require("alpha")
@@ -133,6 +176,13 @@ require("lazy").setup({
       dashboard.opts.layout[1].val = 8
       
       alpha.setup(dashboard.config)
+    end
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup()
     end
   },
   {
